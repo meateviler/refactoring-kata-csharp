608cc4086f98612ef539c5b6ccea6bdacde939bf
meateviler <meateviler@msn.com>
2017-03-13 11:02:54 +0800

    add JasonMaker and Symbol file

---
+++ RefactoringKata/JsonMaker.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace RefactoringKata
+{
+	public class JsonMaker
+	{
+		public JsonMaker()
+		{
+		}
+
+		private static string GetQuterString(string str)
+		{
+			return "\"" + str + "\"";
+		}
+
+		public static string MakeItem(string key, string value)
+		{
+			return MakeItemWithoutCama(key, value) + Symbol.CAMA;
+		}
+
+		public static string MakeItemWithoutCama(string key, string value)
+		{
+			var sb = new StringBuilder(MakeKey(key));
+			return sb.Append(GetQuterString(value)).ToString();
+		}
+
+		public static string MakeItem(string key, decimal value)
+		{
+			var sb = new StringBuilder(MakeKey(key));
+			sb.Append(value);
+			sb.Append(Symbol.CAMA);
+			return sb.ToString();
+		}
+
+		public static string MakeKey(string key)
+		{
+			var sb = new StringBuilder(GetQuterString(key));
+			return sb.Append(Symbol.COLN).ToString();
+		}
+
+		public static string AddCurlyBrackets(string item)
+		{
+			var sb = new StringBuilder();
+			sb.Append(Symbol.LEFT_CURLY_BRACKETS);
+			sb.Append(item);
+			sb.Append(Symbol.RIGHT_CURLY_BRACKETS);
+			return sb.ToString();
+		}
+
+		public static string AddSquareBrackets(string item)
+		{
+			var sb = new StringBuilder();
+			sb.Append(Symbol.LEFT_SQUARE_BRACKETS);
+			sb.Append(item);
+			sb.Append(Symbol.RIGHT_SQUARE_BRACKETS);
+			return sb.ToString();
+		}
+	}
+}
---
+++ RefactoringKata/Symbol.cs
@@ -0,0 +1,12 @@
+namespace RefactoringKata
+{
+	public class Symbol
+	{
+		public static string COLN = ": ";
+		public static string CAMA = ", ";
+		public static string LEFT_CURLY_BRACKETS = "{";
+		public static string RIGHT_CURLY_BRACKETS = "}";
+		public static string LEFT_SQUARE_BRACKETS  = "[";
+		public static string RIGHT_SQUARE_BRACKETS  = "]";
+	}
+}
